2bf931f357fc31e4348f2c7e63c53d5d8309d109
alutab2939 <91492963+alutab2939@users.noreply.github.com>
2022-02-10 01:13:24 +0100

    Update usuarios.sh

--- USUARIOS/usuarios.sh
+++ USUARIOS/usuarios.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+while IFS=: read -r col1 col2 col3 col4 col5 col6 col7 col8
+do
+        echo "I got:$col1|$col2"
+        NOMBRE=$col1
+        APELLIDOS=$col2
+        LOGIN=$col3
+        PASSWORD=$col4
+        DEPARTAMENTO=$col5
+        EMAIL=$col6
+        GRUPO=$col7
+        UO=$col8
 
+        echo -n "Añadiendo usuario $col3..."
+        samba-tool user create $col3 $col4 --must-change-at-next-login --userou=ou=$col8 --home-directory=/home/$col3 --given-name=$col1 --surname=$col2 --mail=$col6 --department=$col5 --login-shell=/bin/bash
+        samba-tool group addmembers $col7 $col3
+        echo "Usuario $col3 creado correctamente"
+
+done < $1
